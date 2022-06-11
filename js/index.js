0e2219bb0854ef6caa7a03d419c9e7d766ef3458
Pablo <olbapdow@hotmail.com>
2022-06-11 18:34:27 +0200

    15% presentation initial

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+const btn = document.getElementById('btn') //1
 const game = new Game(ctx);
+const play = document.getElementById('play')//2
+const canvasGame = document.getElementById('canvas-game')//3
 
-document.getElementById('btn').addEventListener('click', function (e){
+//1
+btn.addEventListener('click', function (e){
     if (game.interval){
         game.stop();
         this.innerText = "START";
@@ -12,3 +16,8 @@ document.getElementById('btn').addEventListener('click', function (e){
         this.innerText = "STOP"
     }
 });
+
+play.addEventListener('click', function(){//2
+    canvasGame.style.display = 'block'//3
+    play.style.display = 'none'//2
+})
